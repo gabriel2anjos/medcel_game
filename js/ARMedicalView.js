cd8a63d4640ba325796a3621723eafb6bed4a4d9
gabriel2anjos <gabriel.anjos.dt@gmail.com>
2019-02-28 12:39:24 -0300

    abc

--- js/ARMedicalView.js
+++ js/ARMedicalView.js
@@ -19,7 +19,8 @@ import {
   ViroText,
   ViroARPlaneSelector,
   ViroNode,
-  ViroSpotLight
+  ViroSpotLight,
+  ViroFlexView
 } from 'react-viro';
 
 export default class ARMedicalView extends Component {
@@ -28,7 +29,10 @@ export default class ARMedicalView extends Component {
         super(props);
         // Set initial state here
         this.state = {
-        text : "Initializing AR...",
+          text : "Initializing AR...",
+          animationName:"mixamo.com",
+          modelAnim: false,
+          loopState:false,
         };
 
         this._onHover = this._onHover.bind(this)
@@ -36,31 +40,38 @@ export default class ARMedicalView extends Component {
 
     render() {
         return (
-          <ViroARScene ref={(component)=>{this.sceneRef = component}} displayPointCloud={true}>
+          <ViroARScene ref={(component)=>{this.sceneRef = component}} displayPointCloud={true} >
           <ViroAmbientLight color="#ffffff" intensity={200}/>
-            <ViroARPlaneSelector>
+            {/* <ViroARPlaneSelector> */}
+            <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+
             <ViroImage
-                height={.1}
-                width={.1}
+                height={.80}
+                width={.80}
                 source={require("./res/floor.jpg")}
                 position={[0,0.0,0]}
                 rotation={[270,0,0]}
               />
+              {/* <ViroFlexView style={styles.titleContainer} position={[0.2, 0.4, -0.1]} rotation={[0, 0, 0]} height={.13} width={.32}> */}
+                {/* <ViroText style={styles.prodTitleText} position={[0.2, 0.7, -0.1]} rotation={[0, 0, 0]} text="AAAAA" width={.32} height={.0325} /> */}
+                <ViroText style={styles.prodDescriptionText} position={[0, 0.42, 0]} width={0.1} height={0.1} scale={[.05, .05, .05]} fontWeight='100' text="Estou com dor na perna" />
+              {/* </ViroFlexView> */}
               <Viro3DObject
-              source={require('./res/eric/MyCharacter.vrx')}
+              source={require('./res/eric/Idle.vrx')}
               resources={[
-                require('./res/eric/MyCharacter_color.jpg'),
+                require('./res/eric/paciente_color.jpg'),
               ]}
-              scale={[0.0025,0.0025,0.0025]}
+              scale={[0.0022,0.0022,0.0022]}
               type='VRX'
               ref={ "person"}
               ignoreEventHandling={true}
+              animation={{name:this.state.animationName, run:true, loop:true, onFinish:this._onFinish,}}
               materials={"pbr"}
               />
               <ViroAmbientLight color="#ffffff" />
               {this._headHitbox()}
-            </ViroARPlaneSelector>
-
+            {/* </ViroARPlaneSelector> */}
+              </ViroARImageMarker>
           </ViroARScene>
         );
     }
@@ -84,6 +95,7 @@ export default class ARMedicalView extends Component {
             this.props.arSceneNavigator.viroAppProps.changeHoverText("");
         }
     }
+
 }
 
 ViroMaterials.createMaterials({
@@ -99,6 +111,29 @@ ViroARTrackingTargets.createTargets({
       physicalWidth : 0.165 // real world width in meters
     }
   });
+
+var styles = StyleSheet.create({
+  prodDescriptionText: {
+    fontFamily: 'sans-serif-light',
+    fontSize: 20,
+    color: 'red',
+    textAlignVertical: 'center',
+    textAlign: 'left',
+    flex: 1,
+  },
+  titleContainer: {
+    flexDirection: 'column',
+    backgroundColor: "#ffffffdd",
+    padding: .2,
+  },
+  prodTitleText: {
+    fontFamily: 'sans-serif-light',
+    fontSize: 30,
+    color: '#222222',
+    textAlignVertical: 'center',
+    textAlign: 'left',
+  },
+});
   
 
 module.exports = ARMedicalView;
